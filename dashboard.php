e7c6940d37c5bb87f914867da93030a77ed5842e
alyaaacoi <you@example.com>
2025-06-20 14:41:58 +0500

    saya juga suda copp

--- dashboard.php
+++ dashboard.php
@@ -4,8 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DASHBOARD | SISTEM MANAGEMENT LAB</title>
+    <link rel="stylesheet" href="dashboard.css">
 </head>
 <body>
-    
+   <?php include "sidebar.php"; ?>
+    <div class="container">
+        <header>
+            <h1>Selamat Datang di Website Sistem Management Lab</h1>
+        </header>
+        <div class="cards-container">
+            <div class="cards">
+                <div class="card">
+                    <h2>Jumlah Peminjam</h2> 
+                    <p>2 orang</p>
+                </div>
+                <div class="card">
+                    <h2>Pengguna Lab</h2> 
+                    <p>2 kelas</p>
+                </div>
+                <div class="card">
+                    <h2>Jumlah kategori barang</h2> 
+                    <p>2 jenis barang</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
