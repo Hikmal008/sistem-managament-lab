3c3998676d275bb9f432d6cf59145e723aab699e
Hikmal008 <syahiyuddinhikmal22@gmail.com>
2025-06-20 15:19:11 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SISTEM MANAGAMENT LAB</title>
+</head>
+<body>
+    <h1>bacootttttt</h1>
+</body>
+</html>
